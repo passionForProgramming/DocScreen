3f09cfb25d667c8e6a70938b4026c4f7bcaf2df9
bryan316i <bryan316i@hotmail.com>
2014-08-19 13:28:48 -0600

    Capturas automaticas - capturas
    Capturas arregladas, ya toma la ventana activa

--- AyDdS1_DocScreen/AyDdS1_DocScreen/PantallaPrincipal.cs
+++ AyDdS1_DocScreen/AyDdS1_DocScreen/PantallaPrincipal.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,6 +36,7 @@ namespace AyDdS1_DocScreen
         }
 
         /*CAPTURAS AUTOMATICAS*/
+
         //boton iniciar
         private void btnCAIniciar_Click(object sender, EventArgs e)
         {
@@ -66,10 +69,17 @@ namespace AyDdS1_DocScreen
                 MessageBoxButtons.OK, MessageBoxIcon.None);
         }
 
+        /*TOMAR CAPTURA*/
+
         //toma captura
         private RegistroScreen capturar( string nombreArchivo, string pathGuardado ){
             //crea un objeto para poner limites
-            Rectangle bounds = this.Bounds;
+            //Rectangle bounds = this.Bounds;
+            Rectangle bounds = new Rectangle();
+            //pide que se llene con la ventana activa
+            leerDimensionVentanaActiva( ref bounds );
+            //muestra dimensiones en consola
+            //Console.WriteLine(String.Format("(x,y) = {0},{1}\t(width,height) = {2},{3}", bounds.X, bounds.Y, bounds.Width, bounds.Height));
             //crea una imagen
             using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height)){
                 using (Graphics g = Graphics.FromImage(bitmap)){
@@ -88,7 +98,46 @@ namespace AyDdS1_DocScreen
             return rs;
         }
 
+        //permite obtener la posicion de una ventana
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+        //struct para mantener la posicion de una ventana
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        //permite obtener un handler de la ventana actual
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        //
+        private void leerDimensionVentanaActiva( ref Rectangle bounds )
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            RECT rct;
+            //if (!GetWindowRect(new HandleRef(this, this.Handle), out rct))
+            if (!GetWindowRect(new HandleRef(this, hwnd), out rct))
+            {
+                MessageBox.Show("Error, no se detecto ventana", titulo,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            bounds.X = rct.Left;
+            bounds.Y = rct.Top;
+            bounds.Width = rct.Right - rct.Left + 1;
+            bounds.Height = rct.Bottom - rct.Top + 1;
+            Console.WriteLine(String.Format("(x,y) = {0},{1}\t(width,height) = {2},{3}", bounds.X, bounds.Y, bounds.Width, bounds.Height));
+        }
+
         /*CAPTURAS MANUALES*/
+
         private void numCARetardo_ValueChanged(object sender, EventArgs e)
         {
             //prueba
