7938aa9727504636e72b7908919b12692afb0fa3
bryan316i <bryan316i@hotmail.com>
2014-08-18 19:20:16 -0600

    Capturas automaticas - Capturas añadidas
    Falta pulir la toma de capturas

--- AyDdS1_DocScreen/AyDdS1_DocScreen/PantallaPrincipal.cs
+++ AyDdS1_DocScreen/AyDdS1_DocScreen/PantallaPrincipal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,10 +15,19 @@ namespace AyDdS1_DocScreen
     public partial class PantallaPrincipal : Form
     {
         //atributos
+        string pathProyecto;
+        List<RegistroScreen> listaScreens;
+        int cantidadScreens;
 
         //constructor
         public PantallaPrincipal(){
+            //default
             InitializeComponent();
+
+            //inicializacion
+            pathProyecto = "C:\\AyDdS1\\Screenshots\\";
+            listaScreens = new List<RegistroScreen>();
+            cantidadScreens = 0;
         }
 
         /*CAPTURAS AUTOMATICAS*/
@@ -34,13 +44,33 @@ namespace AyDdS1_DocScreen
             this.WindowState = FormWindowState.Minimized;
             //hasta que cumpla la cantidad
             for (int i = 0; i < cantidad; i++){
-                capturar();
+                //arma el nombre
+                string nombreAbsoluto = String.Format("{0}{1}{2}.{3}", pathProyecto, "scr", ++cantidadScreens, ".png");
+                //toma captura
+                capturar(nombreAbsoluto);
+                //retardo si no es la ultima
+                if (i<cantidad-1){
+                    Thread.Sleep(intervalo);
+                }
             }
+            //regresar la pantalla
+            this.WindowState = FormWindowState.Normal;
         }
 
-        private void capturar()
-        {
-
+        //toma captura
+        private void capturar( string pathGuardado ){
+            //crea un objeto para poner limites
+            Rectangle bounds = this.Bounds;
+            //crea una imagen
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height)){
+                using (Graphics g = Graphics.FromImage(bitmap)){
+                    //toma la captura
+                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+                }
+                //guarda el archivo
+                //bitmap.Save("C:\\AyDdS1\\Screenshots\\btn2.png", ImageFormat.Jpeg);
+                bitmap.Save(pathGuardado, ImageFormat.Png);
+            }
         }
 
         /*CAPTURAS MANUALES*/
---
+++ AyDdS1_DocScreen/AyDdS1_DocScreen/RegistroScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyDdS1_DocScreen
+{
+    class RegistroScreen
+    {
+        //atributos
+        string nombre;
+        string descripcion;
+        string path;
+
+        //constructor
+        public RegistroScreen()
+        {
+            //inicializa variables
+            nombre = "";
+            descripcion = "";
+            path = "";
+        }
+    }
+}
